b485c319ba88d3c882cadf96d40ae6d70c3dec7c
Pablo <pcasals76@gmail.com>
2021-02-08 19:50:55 -0300

    CP commiteado por pabloluiscasals a las 1612824117221

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -180,7 +180,7 @@ function agregarSimboloExclamacion(str) {
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
-  // Tu código:
+  // Tu código::
   return nombre + " " + apellido;
 }
 
